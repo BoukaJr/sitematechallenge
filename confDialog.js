5d8b1700d0c71e26a1255e6c4aec6005fa21ba1d
BoukaJr <quinnvb@hotmail.com>
2021-05-26 19:59:01 +1000

    Finished coding challenge

--- confDialog.js
+++ confDialog.js
@@ -6,13 +6,25 @@ class confDialog extends HTMLElement {
     connectedCallback() {
         var welcomeMessage = this.attributes.welcomeMessage.value
         this.innerHTML = `
-          <style>
-
-          </style>
-          <button type="button">Click Me</button>
-          <p>${welcomeMessage}</p>
+        <div id="container" style="display:block;">
+            <p>${welcomeMessage}</p>
+            <button type="button" onclick="clickedYes()">Yes</button>
+            <div id='yes' style="display:none;">
+                You clicked yes!
+            </div>
+            <button type="button" onclick="clickedCancel()">Cancel</button>
+        </div>
+        <div id="cancelMessage" style="display:none;"><div>
         `;
       }
 }
 
 customElements.define('dialog-component', confDialog);
+
+function clickedYes(){
+    document.getElementById('yes').style.display = "block";
+}
+function clickedCancel(){
+    document.getElementById('container').style.display = "none";
+    document.getElementById('cancelMessage').style.display = "block";
+}
